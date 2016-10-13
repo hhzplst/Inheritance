943781eea09e54cbbbc992c11f9af199958f7add
Ivy Chang <ivycwq@icloud.com>
2016-10-12 19:59:07 -0700

    adding methods

--- Student.cs
+++ Student.cs
@@ -1,6 +1,21 @@
+using System;
+using System.Collections.Generic;
 
 namespace Inheritance {
   public class Student : Person {
+    private List<Course> courses = new List<Course>();
     public Student(string firstName, string lastName) : base(firstName, lastName) {}
+    public void AddTakenCourse(Course c) {
+      courses.Add(c);
+    }
+    public void PrintTranscript() {
+      PrintHeader();
+      foreach (Course c in courses)
+        c. PrintCourseInfo(); 
+    }
+    private void PrintHeader() {
+      Console.WriteLine("Printing Transcript");
+      Console.WriteLine("{0, -20}{1, 20}", "Title", "Score");
+    }
   }
 }
